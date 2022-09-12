596c03ac186203d97e0757a870fc101125e360d8
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-09-12 15:45:59 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - prints out the alphabet in reverse
- * Return: 0
+ *main - print 0123456789 using putchar
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char a = 'z';
+	int n = '0';
 
-	while (a >= 'a')
+	while (n <= '9')
 	{
-		putchar(a);
-		a--;
+		putchar(n);
+		n++;
 	}
-
 	putchar('\n');
+
 	return (0);
 }
+Footer
