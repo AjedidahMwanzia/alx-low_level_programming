18d4e8b5ea5e080b44c50e4b35643ec01413a831
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2023-02-22 16:22:07 +0300

    updates

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,22 @@
-#include "holberton.h"
+#include "main.h"
+
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
- *
- * Return: Always 0 (Succes)
+ * print_alphabet - Make the alphabet
+ * Return: void
  */
-void print_alphabet(void)
-{
-	int n;
 
-	for (n = 'a'; n <= 'z'; n++)
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(n);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
+
+
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,22 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_last_digit - a function that prints the last digit of a number
- * @nld: number's last digit result
- * Return: value of the last digit
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to be treated
+ * Return: Value of the last digit of number
  */
-int print_last_digit(int nld)
+
+int print_last_digit(int n)
+
 {
-	int pld;
 
-	pld = (nld % 10);
+	int last;
 
-	if (pld < 0)
+	last = n % 10;
+	if (last < 0)
 	{
-		pld = (-1 * pld);
+	last = last * -1;
 	}
-
-	_putchar(pld + '0');
-	return (pld);
+	_putchar(last + '0');
+	return (last);
 }
